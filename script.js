15eb8b81681f7622a60f5f174d04589b93a53b8a
Denis Le <dl.denisle@gmail.com>
2025-02-27 15:43:28 -0500

    Implement color on hover feature

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let container = document.querySelector('.container');
+let container = document.querySelector('.container');
 
-    for (let i = 0; i < 16; i++) {
-        const row = document.createElement('div');
-        row.classList.add('row');
-        for (let j = 0; j < 16; j++) {
-            const column = document.createElement('div');
-            column.classList.add('column');
-            row.appendChild(column);
-        }
-        container.appendChild(row);
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (let j = 0; j < 16; j++) {
+        const column = document.createElement('div');
+        column.classList.add('column');
+        row.appendChild(column);
+
+        const cells = document.querySelectorAll('.column');
+        cells.forEach(cell => {
+            cell.addEventListener('mouseover', () => {
+                cell.classList.add('color');
+            })
+        });
     }
-});
+    container.appendChild(row);
+}
+
 

bb84113e2dcdf31bb52759f4c022df1400fd3e52
Denis Le <dl.denisle@gmail.com>
2025-02-27 16:27:41 -0500

    Implemented grid change feature

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 let container = document.querySelector('.container');
-let size = 16;
 
 // Creates the grid
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
-    side = prompt('enter grid size: ');
+    size = prompt('enter grid size: ');
+    let rows = document.querySelectorAll('.row');
+    rows.forEach(row => {
+        row.remove();
+    });
+    createGrid(size);
 });
 
-for (let i = 0; i < size; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for (let j = 0; j < size; j++) {
-        const column = document.createElement('div');
-        column.classList.add('column');
-        row.appendChild(column);
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < size; j++) {
+            const column = document.createElement('div');
+            column.classList.add('column');
+            row.appendChild(column);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
+
+    // Colors the grid
+    const cells = document.querySelectorAll('.column');
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', () => {
+            cell.classList.add('color');
+        })
+    });
 }
 
-// Colors the grid
-const cells = document.querySelectorAll('.column');
-cells.forEach(cell => {
-    cell.addEventListener('mouseover', () => {
-        cell.classList.add('color');
-    })
-});
+createGrid(16);
